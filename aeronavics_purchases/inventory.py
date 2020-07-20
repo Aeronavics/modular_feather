783b55cbd6295f85dfaeba54894ef262d4a05fb2
James Morritt <jmo146@uclive.ac.nz>
2020-07-20 23:00:30 +1200

    created odoo and ESC directories

--- aeronavics_purchases/inventory.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class productInherit(models.Model):
-    _inherit = 'product.template'
-    
-    material = fields.Char(string='Material')
-    colour = fields.Char(string='Colour')
-    production_method = fields.Char(string='Production Method')
-    thread = fields.Selection(string='Thread', selection=[('yes','Yes'),('no','No'),('na','N/A')], required=True, default='na')
